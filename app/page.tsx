c1dca6c31298e5e982dab17faae160bdb3870ecc
seizeddev <langatmig-97as-pik@icloud.com>
2025-05-19 22:11:58 +0200

    Added a shadcn Button - updated globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
+
 export default function Page() {
   return (
     <main>
       <h1>Hello world!</h1>
+      <Button asChild>
+        <Link href="/login">Login</Link>
+      </Button>
     </main>
   );
 }
